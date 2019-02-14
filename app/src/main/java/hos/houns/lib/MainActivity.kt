586eae0ec415523b89940a283007de70baeb4169
Hospice HOUNSOU <hospice.h@makeba.money>
2019-02-14 13:50:02 +0000

    Moved Group identifier in library

--- app/src/main/java/hos/houns/lib/MainActivity.kt
+++ app/src/main/java/hos/houns/lib/MainActivity.kt
@@ -2,6 +2,8 @@ package hos.houns.lib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import hos.houns.seckeystore.SimpleKeystore
+import timber.log.Timber
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,8 +22,8 @@ class MainActivity : AppCompatActivity() {
         with(SimpleKeystore(this)) {
             Timber.e(getSensitiveData<String>("name"))
             Timber.e(getSensitiveData<Int>("age").toString())
-            Timber.e(getSensitiveData<Date>("date").time.toString())
-            Timber.e(getSensitiveData<MutableList<String>>("list").toString())
+            Timber.e(getSensitiveData<Date>("date")?.time.toString())
+            //Timber.e(getSensitiveData<MutableList<String>>("list").toString())
 
         }
     }
