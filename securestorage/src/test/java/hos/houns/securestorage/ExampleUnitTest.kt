a7aaf47407efc5c8c790f30389fda8e92cceb76d
Hospice HOUNSOU <hospice.h@makeba.money>
2019-07-18 10:36:05 +0000

    Add optional context parameter to init method.

--- securestorage/src/test/java/hos/houns/securestorage/ExampleUnitTest.kt
+++ securestorage/src/test/java/hos/houns/securestorage/ExampleUnitTest.kt
@@ -1,6 +1,11 @@
 package hos.houns.securestorage
 
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.mock
 import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 /**
@@ -9,8 +14,22 @@ import org.junit.Test
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
 class ExampleUnitTest {
+
+    @Rule
+    @JvmField
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private val mcontext = mock<Context>()
+    @Before
+    fun before() {
+        SecureStorage.init(mcontext)
+        SecureStorage.setValue<String>("test", "urrrrrlllllll")
+    }
+
+
     @Test
     fun addition_isCorrect() {
+        SecureStorage.getValue<String>("test")
         assertEquals(4, (2 + 2).toLong())
     }
 }
